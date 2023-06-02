331935e964c1b93f672bf3c41f9355e66c95a9dc
deividvaz1 <deividvaz100@gmail.com>
2023-06-02 13:10:32 -0300

    teste

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -15,11 +15,11 @@ export async function GET(request: NextRequest) {
 
   const redirectURL = redirectTo ?? new URL('/', request.url)
 
-  const cookieExpiresInSeconds = 60 * 60 * 24 * 30
+  const cookieExpiresInSeconds = 60 * 60 * 24 * 14 // 14 days
 
   return NextResponse.redirect(redirectURL, {
     headers: {
-      'Set-Cookie': `token=${token}; Path=/; max-age=${cookieExpiresInSeconds};`,
+      'Set-Cookie': `token=${token}; Path=/; max-age=${cookieExpiresInSeconds}`,
     },
   })
 }
