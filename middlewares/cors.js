16bce6ec5a7cbe6c608eb4a7f6409b424b9f0c7b
deividvaz1 <deividvaz100@gmail.com>
2023-06-05 16:48:34 -0300

    teste

--- middlewares/cors.js
+++
@@ -1,9 +0,0 @@
-const corsMiddleware = (req, res, next) => {
-  res.setHeader(
-    'Access-Control-Allow-Origin',
-    'https://projeto-nlw-five.vercel.app/',
-  )
-  next()
-}
-
-export default corsMiddleware
