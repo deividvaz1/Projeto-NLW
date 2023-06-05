2b7e1f8a99a8761df739c45ee01b3ef40b52beae
deividvaz1 <deividvaz100@gmail.com>
2023-06-05 14:21:57 -0300

    teste

--- src/components/NewMemoryForm.tsx
+++ src/components/NewMemoryForm.tsx
@@ -1,13 +1,14 @@
 'use client'
 
+import { FormEvent, useState } from 'react'
 import { Camera } from 'lucide-react'
-import { MediaPicker } from './MediaPicker'
-import { FormEvent } from 'react'
-import { api } from '@/lib/api'
 import Cookie from 'js-cookie'
+import { MediaPicker } from './MediaPicker'
+import { api } from '@/lib/api'
 import { useRouter } from 'next/navigation'
 
 export function NewMemoryForm() {
+  const [loading, setLoading] = useState(false)
   const router = useRouter()
 
   async function handleCreateMemory(event: FormEvent<HTMLFormElement>) {
@@ -23,12 +24,26 @@ export function NewMemoryForm() {
       const uploadFormData = new FormData()
       uploadFormData.set('file', fileToUpload)
 
-      const uploadResponse = await api.post('/upload', uploadFormData)
+      // @ts-ignore-next-line
+      uploadFormData.set('fileName', fileToUpload.name)
+      uploadFormData.set('folder', 'nlw-spacetime')
 
-      coverUrl = uploadResponse.data.fileUrl
+      const uploadResponse = await api.post(
+        'https://upload.imagekit.io/api/v1/files/upload',
+        uploadFormData,
+        {
+          headers: {
+            Authorization: `Basic ${process.env.NEXT_PUBLIC_IMAGEKIT_PRIVATE_KEY}`,
+          },
+        },
+      )
+
+      coverUrl = uploadResponse.data.url
     }
 
     const token = Cookie.get('token')
+
+    setLoading(true)
 
     await api.post(
       '/memories',
@@ -44,11 +59,15 @@ export function NewMemoryForm() {
       },
     )
 
-    router.push('/')
+    router.replace('/?success')
   }
 
   return (
-    <form onSubmit={handleCreateMemory} className="flex flex-1 flex-col gap-2">
+    <form
+      onSubmit={handleCreateMemory}
+      className="flex flex-1 flex-col gap-2"
+      id="form"
+    >
       <div className="flex items-center gap-4">
         <label
           htmlFor="media"
@@ -57,7 +76,6 @@ export function NewMemoryForm() {
           <Camera className="h-4 w-4" />
           Anexar mídia
         </label>
-
         <label
           htmlFor="isPublic"
           className="flex items-center gap-1.5 text-sm text-gray-200 hover:text-gray-100"
@@ -66,25 +84,25 @@ export function NewMemoryForm() {
             type="checkbox"
             name="isPublic"
             id="isPublic"
-            value="true"
+            value="false"
             className="h-4 w-4 rounded border-gray-400 bg-gray-700 text-purple-500"
           />
           Tornar memória pública
         </label>
       </div>
-
       <MediaPicker />
-
       <textarea
         name="content"
         spellCheck={false}
         className="w-full flex-1 resize-none rounded border-0 bg-transparent p-0 text-lg leading-relaxed text-gray-100 placeholder:text-gray-400 focus:ring-0"
         placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."
       />
-
       <button
         type="submit"
-        className="inline-block self-end rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
+        disabled={loading}
+        className={`inline-block self-end rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600 ${
+          loading && 'cursor-not-allowed'
+        }`}
       >
         Salvar
       </button>
