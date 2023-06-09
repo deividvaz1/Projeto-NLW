f311c448b4bcfe17b82f6e382fa25a574dbee949
deividvaz1 <deividvaz100@gmail.com>
2023-06-09 14:07:14 -0300

    teste

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,7 +9,7 @@ export function Hero() {
 
       <div className="max-w-[420px] space-y-1">
         <h1 className=" text-5xl font-bold leading-tight text-gray-50">
-          Sua cápsula do tempo1
+          Sua cápsula do tempo
         </h1>
         <p className="text-lg leading-relaxed">
           Colecione momentos marcantes da sua jornada e compartilhe (se quiser)
